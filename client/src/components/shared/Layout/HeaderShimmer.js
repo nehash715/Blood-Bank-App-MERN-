02de9ac26a31d6d4481c917089b876573e310701
nehash715 <nehash715@gmail.com>
2024-03-20 02:40:14 +0530

    first

---
+++ client/src/components/shared/Layout/HeaderShimmer.js
@@ -0,0 +1,54 @@
+import React from "react";
+import "./header.css";
+const HeaderShimmer = () => {
+  
+  
+   
+  
+ 
+  return (
+    <>
+      <nav className="navbar1">
+        <div className="container-fluid">
+          <div className="logo1"></div>
+          <div className="h1-shimmer"> </div>
+
+<div className="user1"></div>
+          <div className="user"></div>
+          <div className="shimmer-btn"></div>
+        </div>
+      </nav>
+<div className="body1">
+<div className="sidebar-shimmer">
+  <div className="side-flex1">
+       <div className="org1"></div><div className="org1-text"></div>
+  </div>
+<div className="side-flex">
+  <div className="org1"></div><div className="org1-text"></div>
+  </div>
+  <div className="side-flex">
+  <div className="org1"> </div><div className="org1-text"></div>
+  </div>
+
+
+
+</div>
+<div  className="menuss">
+  <div  className="line"></div>
+  <div className="line" ></div>
+  <div className="line" ></div>
+  <div className="line" ></div>
+  <div className="line" ></div>
+  <div className="line" ></div>
+  <div className="line" ></div>
+  <div className="line" ></div>
+  <div className="line" ></div>
+  
+</div>
+
+</div>
+    </>
+  );
+};
+
+export default HeaderShimmer;
